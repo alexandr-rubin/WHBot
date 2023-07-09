fa3c9fb30461ee2dea37b9bb391deb3cae3e75bd
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 21:21:27 +0300

    last

--- src/helpers/startWebhook.ts
+++ src/helpers/startWebhook.ts
@@ -8,7 +8,7 @@ export const webhookApp = express()
 
 webhookApp.use(express.json())
 
-webhookApp.post('/' + env.TOKEN, async (req, res) => {
+webhookApp.post('/webhook', async (req, res) => {
   // Обработка полученного обновления
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   await bot.handleUpdate(req.body)
