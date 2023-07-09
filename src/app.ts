c04cc00e5d9cf6c8834fb592ae8e9048236fb0d2
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 18:56:25 +0300

    try

--- src/app.ts
+++ src/app.ts
@@ -59,7 +59,6 @@ async function runApp() {
   // await bot.start()
   run(bot)
   console.info(`Bot ${bot.botInfo.username} is up and running`)
-  // webhookApp.use(webhookCallback(bot, 'express'))
   webhookApp.listen(4242, () => console.log('Running on port 4242'))
 }
 
--- src/helpers/startWebhook.ts
+++ src/helpers/startWebhook.ts
@@ -1,9 +1,12 @@
+import { webhookCallback } from 'grammy'
+import bot from './bot'
 import express from 'express'
 
 // eslint-disable-next-line import/prefer-default-export
 export const webhookApp = express()
 
 webhookApp.use(express.json())
+webhookApp.use(webhookCallback(bot, 'express'))
 
 webhookApp.post('/', (req, res) => {
   res.send('hello suk')
