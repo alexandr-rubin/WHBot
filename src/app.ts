9aae3c6c4ee9e2bfc3af6fa45eb47de85ac3e0c3
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 18:48:44 +0300

    qwe

--- src/app.ts
+++ src/app.ts
@@ -60,9 +60,9 @@ async function runApp() {
   //run(bot)
   console.info(`Bot ${bot.botInfo.username} is up and running`)
   //webhookApp.use(webhookCallback(bot, 'express'))
-  //webhookApp.listen(4242, () => console.log('Running on port 4242'))
+  webhookApp.listen(4242, () => console.log('Running on port 4242'))
 }
 
 //if (Cluster.isPrimary) {
-//void runApp()
+// void runApp()
 //}
