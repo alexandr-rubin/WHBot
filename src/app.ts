b0cc29264275a4b5226db2402360023fc3bef7d0
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 17:50:36 +0300

    try

--- src/app.ts
+++ src/app.ts
@@ -56,10 +56,11 @@ async function runApp() {
   // for (const update of updates) {
   //   await bot.handleUpdate(update)
   // }
+  await bot.start()
   //run(bot)
   console.info(`Bot ${bot.botInfo.username} is up and running`)
-  webhookApp.use(webhookCallback(bot, 'express'))
-  webhookApp.listen(4242, () => console.log('Running on port 4242'))
+  // webhookApp.use(webhookCallback(bot, 'express'))
+  // webhookApp.listen(4242, () => console.log('Running on port 4242'))
 }
 
 //if (Cluster.isPrimary) {
