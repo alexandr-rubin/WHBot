9f77aebb8198551a5cecc20ff6860f76f5738384
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 16:50:00 +0300

    add ignoeOldMessageUpdates middleware

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import configureI18n from './middlewares/configureI18n'
 import handleHelp from './handlers/help'
 import handleLanguage from './handlers/language'
 import i18n from './helpers/i18n'
+import ignoreOldMessageUpdates from './middlewares/ignoreOldMessageUpdates'
 import languageMenu from './menus/language'
 import motivate from './handlers/motivate'
 import startMongo from './helpers/startMongo'
@@ -25,6 +26,7 @@ async function runApp() {
   console.log('Mongo connected')
   bot
     // Middlewares
+    .use(ignoreOldMessageUpdates)
     .use(sequentialize())
     .use(ignoreOld())
     .use(attachUser)
@@ -44,42 +46,13 @@ async function runApp() {
   // Start bot
   await bot.init()
   // // Удаление активного вебхука
-  await bot.api.deleteWebhook()
+  // await bot.api.deleteWebhook()
 
   // // Получение обновлений через метод getUpdates
   // const updates = await bot.api.getUpdates()
   run(bot)
   console.info(`Bot ${bot.botInfo.username} is up and running`)
   webhookApp.use(webhookCallback(bot, 'express'))
-
-  // webhookApp.post('/webhook', async (req, res) => {
-  //   const { body } = req
-  //   // Проверка, является ли входящий запрос от Telegram
-  //   if (!body || !body.message) {
-  //     res.sendStatus(200)
-  //     return
-  //   }
-
-  //   // Извлечение информации из входящего сообщения
-  //   const message: Message = body.message
-  //   const chatId = message.chat.id
-  //   const text = message.text
-
-  //   // Обработка команд от пользователя
-  //   if (text === '/start') {
-  //     await bot.api.sendMessage(chatId, 'Привет! Добро пожаловать!')
-  //   } else if (text === '/help') {
-  //     await bot.api.sendMessage(chatId, 'Нужна помощь?')
-  //   } else {
-  //     await bot.api.sendMessage(
-  //       chatId,
-  //       'Неизвестная команда. Попробуйте ещё раз.'
-  //     )
-  //   }
-
-  //   res.sendStatus(200)
-  // })
-
   webhookApp.listen(4242, () => console.log('Running on port 4242'))
 }
 
---
+++ src/middlewares/ignoreOldMessageUpdates.ts
@@ -0,0 +1,26 @@
+import { NextFunction } from 'grammy'
+import Context from '../models/Context'
+
+const threshold = 5 * 60 // 5 minutes
+export default function ignoreOldMessageUpdates(
+  ctx: Context,
+  next: NextFunction
+) {
+  // Check if context update type is a message
+  if (!ctx || !ctx.from || !ctx.chat) {
+    return next()
+  }
+  if (ctx.message) {
+    if (new Date().getTime() / 1000 - ctx.message.date < threshold) {
+      return next()
+    } else {
+      console.log(
+        `Ignoring message from ${ctx.from.id} at ${ctx.chat.id} (${
+          new Date().getTime() / 1000
+        }:${ctx.message.date})`
+      )
+    }
+  } else {
+    return next()
+  }
+}
