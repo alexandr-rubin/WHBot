efd448714fc2eed461b53d0b407635708d75a98f
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 17:05:14 +0300

    fixiki

--- src/app.ts
+++ src/app.ts
@@ -53,7 +53,7 @@ async function runApp() {
   // const updates = await bot.api.getUpdates()
   run(bot)
   console.info(`Bot ${bot.botInfo.username} is up and running`)
-  //webhookApp.use(webhookCallback(bot, 'express'))
+  webhookApp.use(webhookCallback(bot, 'express'))
   webhookApp.listen(4242, () => console.log('Running on port 4242'))
 }
 
