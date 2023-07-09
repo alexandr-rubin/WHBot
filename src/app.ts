fdd0cb9895c8deedf15cf2b82963da920344feb7
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 18:43:11 +0300

    qwerty

--- src/app.ts
+++ src/app.ts
@@ -47,15 +47,15 @@ async function runApp() {
   // Start bot
   await bot.init()
   // Удаление активного вебхука
-  //await bot.api.deleteWebhook()
+  await bot.api.deleteWebhook()
 
-  // // Получение обновлений через метод getUpdates
-  // const updates = await bot.api.getUpdates()
+  // Получение обновлений через метод getUpdates
+  const updates = await bot.api.getUpdates()
 
-  // // Обработка полученных обновлений
-  // for (const update of updates) {
-  //   await bot.handleUpdate(update)
-  // }
+  // Обработка полученных обновлений
+  for (const update of updates) {
+    await bot.handleUpdate(update)
+  }
   await bot.start()
   //run(bot)
   console.info(`Bot ${bot.botInfo.username} is up and running`)
