8a71f7ef639b9023f0bc08e9e7ac40c66bf5cdc4
Alexander Rubin <alexandrrubin0715@gmail.com>
2023-07-09 17:41:45 +0300

    qwe

--- src/app.ts
+++ src/app.ts
@@ -58,7 +58,7 @@ async function runApp() {
   }
   //run(bot)
   console.info(`Bot ${bot.botInfo.username} is up and running`)
-  // webhookApp.use(webhookCallback(bot, 'express'))
+  webhookApp.use(webhookCallback(bot, 'express'))
   webhookApp.listen(4242, () => console.log('Running on port 4242'))
 }
 
