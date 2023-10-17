a0a71912a061d1a51c862c0424b07adf3cc1a230
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-10-18 00:25:00 +0300

    kidala flutt

--- src/handlers/kidala.ts
+++ src/handlers/kidala.ts
@@ -2,7 +2,7 @@ import Context from '../models/Context'
 
 export default async function kidala(ctx: Context) {
   if (ctx.message && ctx.chat) {
-    if (ctx.message.from.username === 'rubangru') {
+    if (ctx.message.from.username === 'fluttafy') {
       await ctx.api.sendMessage(ctx.chat?.id, '↑ кидала ↑')
     }
   }
