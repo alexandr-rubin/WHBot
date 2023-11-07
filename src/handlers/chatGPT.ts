040b6028aa174ace8961fa865ef8f0eb81f5fe00
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-11-07 15:20:47 +0300

    add eror handler to chatGtp

--- src/handlers/chatGPT.ts
+++ src/handlers/chatGPT.ts
@@ -1,19 +1,34 @@
 import Context from '../models/Context'
+import OpenAI from 'openai'
 import openai from '../helpers/openai'
 
 export default async function chatGPT(ctx: Context) {
   if (ctx.message) {
     const message = ctx.message.text
 
-    const chatCompletion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: message ?? 'hello' }],
-      max_tokens: 50,
-    })
+    try {
+      const chatCompletion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: message ?? 'hello' }],
+        max_tokens: 50,
+      })
 
-    const responseMessage: string | null =
-      chatCompletion.choices[0].message.content
+      const responseMessage: string | null =
+        chatCompletion.choices[0].message.content
 
-    return ctx.reply(responseMessage ?? 'no response :(')
+      return ctx.reply(responseMessage ?? 'no response :(')
+    } catch (error) {
+      if (error instanceof OpenAI.APIError) {
+        console.error(error.status) // e.g. 401
+        console.error(error.message) // e.g. The authentication token you passed was invalid...
+        console.error(error.code) // e.g. 'invalid_api_key'
+        console.error(error.type) // e.g. 'invalid_request_error'
+        return ctx.reply(error.message)
+      } else {
+        // Non-API error
+        console.log(error)
+        return ctx.reply('Non-API error')
+      }
+    }
   }
 }
