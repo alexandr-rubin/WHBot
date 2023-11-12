aba9ddddec7b83fee33fc4c28774e3101e06b825
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-11-13 00:43:33 +0300

    increasePiCount login

--- src/handlers/increasePiCount.ts
+++ src/handlers/increasePiCount.ts
@@ -3,8 +3,11 @@ import Context from '../models/Context'
 
 export default async function increasePiCount(ctx: Context) {
   if (ctx.message && ctx.message.text) {
-    console.log(ctx.message.text)
-    await increasePi(ctx.message.text)
-    return await ctx.reply(ctx.message.text)
+    const login = ctx.message.text.split(' ')
+    const user = await increasePi(login[1])
+    if (user) {
+      return await ctx.reply('vi napizdeli uje ' + user.piCount + ' raz(a)')
+    }
+    return await ctx.reply('takogo pizdabola netu')
   }
 }
