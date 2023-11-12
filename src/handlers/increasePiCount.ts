112ea642ca5aa46beb56fe2ac544122f58d4499b
Rubin Alexandr <alexandrrubin0715@gmail.com>
2023-11-12 23:06:47 +0300

    fix

--- src/handlers/increasePiCount.ts
+++ src/handlers/increasePiCount.ts
@@ -3,6 +3,8 @@ import Context from '../models/Context'
 
 export default async function increasePiCount(ctx: Context) {
   if (ctx.message && ctx.message.text) {
+    console.log(ctx.message.text)
     await increasePi(ctx.message.text)
+    return await ctx.reply(ctx.message.text)
   }
 }
