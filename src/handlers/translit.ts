e1aa6001cb1e4809572e6322b056c06a1fe95030
Rubin Alexandr <alexandrrubin0715@gmail.com>
2024-01-09 15:53:57 +0300

    fix: all text convert

--- src/handlers/translit.ts
+++ src/handlers/translit.ts
@@ -5,11 +5,14 @@ export default async function replaceWithRussianLayout(ctx: Context) {
   if (ctx.message && ctx.message.text) {
     const message = ctx.message.text.split(' ')
 
+    // fix
     if (message.length < 2 || message[1].trim() === '') {
       return
     }
+
     const layoutMapping = getRussianLayoutMapping()
-    const resultArray = message[1]
+    const messageText = message.slice(1).join('')
+    const resultArray = messageText
       .split('')
       .map((char) => layoutMapping[char] || char)
     const result = resultArray.join('')
