35d3724a7dd078bb684e52be2c501ac05944bd66
Alexander Rubin <alexandrrubin0715@gmail.com>
2025-06-04 16:30:39 +0300

    add ahaha log

--- src/handlers/AHAHA.ts
+++ src/handlers/AHAHA.ts
@@ -2,5 +2,6 @@ import Context from '../models/Context'
 import ahahahaha from '../helpers/ahaha'
 
 export default async function ahaha(ctx: Context) {
+  console.log('AHAHA handler called')
   return await ctx.reply(ahahahaha())
 }
