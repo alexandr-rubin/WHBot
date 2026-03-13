a633f415ff2e3702972f35c1e2e796902a5f32b1
Alexander Rubin <alexandrrubin0715@gmail.com>
2026-03-13 17:48:32 +0300

    fix attachUser

--- src/middlewares/attachUser.ts
+++ src/middlewares/attachUser.ts
@@ -4,7 +4,8 @@ import Context from '../models/Context'
 
 export default async function attachUser(ctx: Context, next: NextFunction) {
   if (!ctx.from) {
-    throw new Error('No from field found')
+    // throw new Error('No from field found')
+    return next()
   }
   const userName = ctx.from.username ?? 'noUsername'
   const user = await findOrCreateUser(ctx.from.id, userName)
